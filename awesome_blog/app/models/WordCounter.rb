0c8e512dc0104f19a5959696fdbbce331c0e94f1
Elvis Ibarra <ielvisd@gmail.com>
2020-02-03 14:45:05 -0800

    Adds functionality to WordCounter module and included it into the Quote and Post model files

--- awesome_blog/app/models/WordCounter.rb
+++ awesome_blog/app/models/WordCounter.rb
@@ -1,2 +1,8 @@
 module WordCounter
+  def word_count
+    # NOTE: Returns the number of words in the body attribute (should be a string)
+    if self.body == nil
+        return 0
+    return self.body.split.size
+  end
 end
